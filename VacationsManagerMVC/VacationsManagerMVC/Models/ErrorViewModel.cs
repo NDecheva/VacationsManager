b95764b1bcd58e242ebdac1394be77d8fdf94e1c
Petkata23 <petarh06@gmail.com>
2024-11-12 16:49:00 +0200

    Created the structure of the project.

---
+++ VacationsManagerMVC/VacationsManagerMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VacationsManagerMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
