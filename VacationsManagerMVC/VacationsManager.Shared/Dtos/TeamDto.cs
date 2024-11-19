dac38ab1e9d2e561748fca451da488f7be6369c4
Misho198 <mi6ooooo@abv.bg>
2024-11-19 20:29:11 +0200

    Created Dtos and constants

---
+++ VacationsManagerMVC/VacationsManager.Shared/Dtos/TeamDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VacationsManager.Shared.Dtos
+{
+    public class TeamDto : BaseModel
+    {
+        public TeamDto()
+        {
+            Developers = new List<UserDto>();
+        }
+
+        public string Name { get; set; }
+        public ProjectDto Project { get; set; }
+        public UserDto TeamLeader { get; set; }
+        public List<UserDto> Developers { get; set; }
+    }
+}
