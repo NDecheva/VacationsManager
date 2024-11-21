9325619a0801f1142fafe872c4e621727f97eda3
krisilozeva <krisi.lozeva@gmail.com>
2024-11-21 12:37:11 +0200

    Added interfaces of repositories.

---
+++ VacationsManagerMVC/VacationsManager.Shared/Repos/Contracts/IRoleRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VacationsManager.Shared.Dtos;
+using YourNamespace.Shared.Repos.Contracts;
+
+namespace VacationsManager.Shared.Repos.Contracts
+{
+    public interface IRoleRepository : IBaseRepository<RoleDto>
+    {
+        Task<RoleDto?> GetByNameIfExistsAsync(string name);
+    }
+}
---
+++ VacationsManagerMVC/VacationsManager.Shared/Repos/Contracts/IVacationRequestRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VacationsManager.Shared.Dtos;
+using YourNamespace.Shared.Repos.Contracts;
+
+namespace VacationsManager.Shared.Repos.Contracts
+{
+    public interface IVacationRequestRepository:IBaseRepository<VacationRequestDto>
+    {
+    }
+}
