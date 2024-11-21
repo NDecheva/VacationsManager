04ab412516509ba361f403015ce31991f77f3ff7
krisilozeva <krisi.lozeva@gmail.com>
2024-11-21 14:07:36 +0200

    added UserRepository

---
+++ VacationsManagerMVC/VacationsManager.Data/Repos/UserRepository.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VacationsManager.Data.Entities;
+using VacationsManager.Shared.Attributes;
+using VacationsManager.Shared.Dtos;
+using VacationsManager.Shared.Repos.Contracts;
+using VacationsManager.Shared.Security;
+using YourNamespace.Data.Repos;
+
+namespace VacationsManager.Data.Repos
+{
+    [AutoBind]
+
+    public class UserRepository : BaseRepository<User, UserDto>, IUserRepository
+    {
+        public UserRepository(VacationsManagerDbContext context, IMapper mapper) : base(context, mapper)
+        {
+        }
+
+        public async Task<bool> CanUserLoginAsync(string username, string password)
+        {
+            var userEntity = await _dbSet.FirstOrDefaultAsync(u => u.Username == username);
+
+            if (userEntity == null)
+            {
+                return false;
+            }
+
+            return PasswordHasher.VerifyPassword(password, userEntity.Password);
+        }
+
+        public async Task<UserDto> GetByUsernameAsync(string username)
+        {
+            return MapToModel(await _dbSet.FirstOrDefaultAsync(u => u.Username == username));
+        }
+    }
+}
