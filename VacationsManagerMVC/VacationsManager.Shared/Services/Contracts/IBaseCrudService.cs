9396cc3ff588e3df1d91e33335244a5dd22829a9
Misho198 <mi6ooooo@abv.bg>
2024-11-26 12:14:14 +0200

    Added BaseCrudService and IBaseCrudService

---
+++ VacationsManagerMVC/VacationsManager.Shared/Services/Contracts/IBaseCrudService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VacationsManager.Shared.Dtos;
+using YourNamespace.Shared.Repos.Contracts;
+
+namespace VacationsManager.Shared.Services.Contracts
+{
+    public interface IBaseCrudService<TModel, TRepository>
+          where TModel : BaseModel
+          where TRepository : IBaseRepository<TModel>
+    {
+        Task<IEnumerable<TModel>> GetAllAsync();
+        Task<TModel> GetByIdIfExistsAsync(int id);
+        Task SaveAsync(TModel model);
+        Task DeleteAsync(int id);
+        Task<IEnumerable<TModel>> GetWithPaginationAsync(int pageSize, int pageNumber);
+        Task<bool> ExistsByIdAsync(int id);
+    }
+}
