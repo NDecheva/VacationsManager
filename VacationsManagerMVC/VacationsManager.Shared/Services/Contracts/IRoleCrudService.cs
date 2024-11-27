2889710a70740498fab9423653ab76f1acffc161
krisilozeva <krisi.lozeva@gmail.com>
2024-11-27 08:26:25 +0200

    VM-32 Added interfaces of services

---
+++ VacationsManagerMVC/VacationsManager.Shared/Services/Contracts/IRoleCrudService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VacationsManager.Shared.Dtos;
+using VacationsManager.Shared.Repos.Contracts;
+
+namespace VacationsManager.Shared.Services.Contracts
+{
+    public interface IRoleCrudService : IBaseCrudService<RoleDto, IRoleRepository>
+
+    {
+        Task<RoleDto?> GetByNameIfExistsAsync(string v);
+    }
+}
---
+++ VacationsManagerMVC/VacationsManager.Shared/Services/Contracts/IUserCrudService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VacationsManager.Shared.Dtos;
+using VacationsManager.Shared.Repos.Contracts;
+
+namespace VacationsManager.Shared.Services.Contracts
+{
+    public interface IUserService : IBaseCrudService<UserDto, IUserRepository>
+    {
+        Task<bool> CanUserLoginAsync(string username, string password);
+        Task<UserDto> GetByUsernameAsync(string username);
+    }
+}
