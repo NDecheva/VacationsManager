a040d8bf90bc42acd39a88bb02681e78607d56be
Petkata23 <petarh06@gmail.com>
2024-11-27 09:39:49 +0200

    VM-37 added some VMs

---
+++ VacationsManagerMVC/VacationsManagerMVC/ViewModels/ProjectEditVM.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace VacationsManagerMVC.ViewModels
+{
+    public class ProjectEditVM
+    {
+        [DisplayName("Project Name")]
+        [Required(ErrorMessage = "Project name is required.")]
+        public string Name { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayName("Start Date")]
+        public DateTime? StartDate { get; set; }
+
+        [DisplayName("Description")]
+        [Required(ErrorMessage = "Description is required.")]
+        public string Description { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayName("End Date")]
+        public DateTime? EndDate { get; set; }
+
+        [DisplayName("Is Completed")]
+        public bool IsCompleted { get; set; }
+
+        // Списък с екипите в проекта
+        public List<TeamDetailsVM> Teams { get; set; }
+    }
+}
