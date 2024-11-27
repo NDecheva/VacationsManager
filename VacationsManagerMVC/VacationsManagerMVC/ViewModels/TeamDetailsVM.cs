9ee6791eeb287815b2d0ad3c5331a68ec937748f
Petkata23 <petarh06@gmail.com>
2024-11-27 09:39:49 +0200

    VM-37 added some VMs

---
+++ VacationsManagerMVC/VacationsManagerMVC/ViewModels/TeamDetailsVM.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+
+namespace VacationsManagerMVC.ViewModels
+{
+    public class TeamDetailsVM
+    {
+        public int TeamId { get; set; }
+
+        [DisplayName("Team Name")]
+        public string Name { get; set; }
+
+        [DisplayName("Project Name")]
+        public string ProjectName { get; set; }
+
+        [DisplayName("Team Leader")]
+        public string TeamLeader { get; set; }
+
+        [DisplayName("Developers")]
+        public List<string> Developers { get; set; } 
+
+        public TeamDetailsVM()
+        {
+            Developers = new List<string>();
+        }
+    }
+}
