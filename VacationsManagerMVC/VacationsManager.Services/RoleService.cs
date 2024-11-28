8e7359dd4c06ab124794e0cec2cba0a1468c989d
krisilozeva <krisi.lozeva@gmail.com>
2024-11-28 13:00:07 +0200

    VM-33 Created services

---
+++ VacationsManagerMVC/VacationsManager.Services/RoleService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VacationsManager.Shared.Attributes;
+using VacationsManager.Shared.Dtos;
+using VacationsManager.Shared.Repos.Contracts;
+using VacationsManager.Shared.Services.Contracts;
+
+namespace VacationsManager.Services
+{
+    [AutoBind]
+    public class RoleService : BaseCrudService<RoleDto, IRoleRepository>, IRoleService
+    {
+        public RoleService(IRoleRepository repository) : base(repository)
+        {
+
+        }
+        public Task<RoleDto?> GetByNameIfExistsAsync(string name)
+        {
+            return _repository.GetByNameIfExistsAsync(name);
+
+        }
+
+    }
+}
---
+++ VacationsManagerMVC/VacationsManager.Shared/Services/Contracts/INotificationService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VacationsManager.Shared.Dtos;
+using VacationsManager.Shared.Repos.Contracts;
+
+namespace VacationsManager.Shared.Services.Contracts
+{
+    public interface INotificationService : IBaseCrudService<NotificationDto, INotificationRepository> 
+    {
+    }
+}
