3ba93a2c315e1bf6cd1d10ce27564e1bd78a41dd
ilian920 <ilianpetkov8@gmail.com>
2024-12-17 13:04:11 +0200

    VM-39 fixed TeamController

--- VacationsManagerMVC/VacationsManager.Data/Repos/UserRepository.cs
+++ VacationsManagerMVC/VacationsManager.Data/Repos/UserRepository.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -37,6 +38,34 @@ namespace VacationsManager.Data.Repos
         public async Task<UserDto> GetByUsernameAsync(string username)
         {
             return MapToModel(await _dbSet.FirstOrDefaultAsync(u => u.Username == username));
+        }
+
+        public async Task<IEnumerable<UserDto>> GetFreeTeamLeadersAsync()
+        {
+            var query = @"
+        SELECT u.Id, u.FirstName, u.LastName
+        FROM dbo.Users u
+        WHERE u.Id NOT IN (
+            SELECT DISTINCT t.TeamLeaderId 
+            FROM dbo.Teams t
+        )
+        AND u.RoleId = @TeamLeaderRoleId
+    ";
+
+            // Execute the query using FromSqlRaw, ensuring it's recognized as part of the Users DbSet.
+            var teamLeaders = await _context.Set<User>()
+                .FromSqlRaw(query, new SqlParameter("@TeamLeaderRoleId", 3)) // Assuming RoleId = 3 is for Team Leaders
+                .ToListAsync();
+
+            // Map the result to DTO
+            var teamLeaderDtos = teamLeaders.Select(u => new UserDto
+            {
+                Id = u.Id,
+                FirstName = u.FirstName,
+                LastName = u.LastName
+            }).ToList();
+
+            return teamLeaderDtos;
         }
     }
 }
